f28cd1cc83e9189c51e8471f2a87a905336b6fdd
Gloria Curran <glocurran2@gmail.com>
2025-10-29 17:14:49 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html class="no-js no-touch" lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Gloria Curran Portfolio</title>
+  <meta name="description" content="portfolio site">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+  <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+  <link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Euphoria+Script' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="css/gcstyles.css">
+  <!--[if lt IE 9]>
+<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+
+<body>
+  <?php include_once("analyticstracking.php") ?>
+  <div id="container">
+    <header>
+
+      <div id="sitename">
+        gloria curran
+      </div>
+      <div id="tagline">portfolio</div>
+
+    </header>
+
+    <nav>
+      <ul>
+        <li><a href="mailto:glocurran7@gmail.com">contact me!</a></li>
+      </ul>
+    </nav>
+
+
+    <section id="content">
+      <div class="intro">
+        <div class="subhead">some examples...</div>
+        <p>Specializing in website prototyping, web design, and development technologies. </p>
+        <p>Prototyping/Design: Adobe XD, Figma, Photoshop, Illustrator.</p>
+        <p>Technologies: (Front-end) VUE JS 3, HTML5, CSS3, Bootstrap, SASS, LESS, JavaScript, TypeScript, jQuery, AJAX, JSON, XML, WordPress CMS, (Back-end) PHP, ColdFusion, SQL Server, MySQL, MariaDB, Oracle</p>
+      </div>
+
+
+      <div class="featured">
+
+        <div class="flex-container">
+
+          <div class="sample">
+            <h2>MVC Global &ndash; Web Design and Coding</h2>
+            <div class="clearer"></div>
+            <a href="https://medicalvaluechain.com/en/" target="_blank"><img src="images/mvc2.png" alt="mvc global "></a>
+            <div class="flyout2" onClick="openWin('https://medicalvaluechain.com/en/');"><span class="descriptions">MVC Global (Blockchain Service) &mdash; Complete WordPress design and development using JavaScript, CSS, and PHP.</span></div>
+            <div class="clearer"></div>
+            <div class="mobile-text">MVC Global (Blockchain Service) &mdash; Complete WordPress design and development using JavaScript, CSS, and PHP.</div>
+          </div>
+
+
+          <div class="sample">
+            <h2>Sanofi Pasteur &ndash; Ecomm Redesign Project Lead</h2>
+            <div class="clearer"></div>
+            <a href="https://www.vaccineshoppe.com" target="_blank"><img src="images/vacccine-shoppe.png" width="280" alt="vaccine shoppe"></a>
+            <div class="flyout2" onClick="openWin('https://www.vaccineshoppe.com');"><span class="descriptions">Project lead for the implementation of the redesign of Sanofi Pasteur's VaccineShoppe.com e-commerce site using ColdFusion, HTML5, JavaScript, JQuery, CSS3.</span></div>
+            <div class="clearer"></div>
+            <div class="mobile-text">Project lead for the implementation of the redesign of Sanofi Pasteur's VaccineShoppe.com e-commerce site using ColdFusion, HTML5, JavaScript, JQuery, CSS3.</div>
+          </div>
+
+          <div class="sample">
+            <h2>Landing Page Design for Sony</h2>
+            <div class="clearer"></div>
+            <a href="https://www.gloriacurran.com/sony/sony1.html" target="_blank"><img src="images/sony_memory_cards.jpg" width="280" alt="Sanofi Pasteur website redesign project"></a>
+            <div class="flyout2" onClick="openWin('https://www.gloriacurran.com/sony/sony1.html');"><span class="descriptions">Design of Landing Page and original artwork for Sony Professional Products. (Created reflection effect beneath memory cards using Photoshop).</span></div>
+            <div class="clearer"></div>
+            <div class="mobile-text">Design of Landing Page and original artwork for Sony Professional Products. (Created reflection effect beneath memory cards using Photoshop).</div>
+          </div>
+
+          <div class="spcr"></div>
+
+          <div class="sample">
+            <h2>The Story behind Sony Pro-Media Landing Page</h2>
+            <div class="clearer"></div>
+            <a href="https://www.gloriacurran.com/prototypes/sony-landing-page-design.html" target="_blank"><img src="images/prototyping-about-sony.jpg" width="280" height="233" alt="About the Creation of Sony Pro-Media Landing Page"></a><br>
+            <div class="flyout2" onClick="openWin('https://www.gloriacurran.com/prototypes/sony-landing-page-design.html');"><span class="descriptions">Story behind the design of Sony's Pro-Media Landing Page and style choices.</span></div>
+            <div class="clearer"></div>
+            <div class="mobile-text">Story behind the design of Sony's Pro-Media Landing Page and style choices.</div>
+          </div>
+
+          <div class="sample">
+            <h2>Sony Real Screen E-Blast &ndash; Design and Coding</h2>
+            <div class="clearer"></div>
+            <a href="https://www.gloriacurran.com/sony/sony3.html" target="_blank"><img src="images/sony_real_screen.jpg" width="280" height="233" alt="sony real screen eblast"></a>
+            <div class="flyout2" onClick="openWin('https://www.gloriacurran.com/sony/sony3.html');"><span class="descriptions">Design and HTML/CSS Coding of EBlast for Sony's annual appearance at the Real Screen Conference located in Las Vegas targeting the Broadcasting sector.</span></div>
+            <div class="clearer"></div>
+            <div class="mobile-text">Design and HTML/CSS Coding of EBlast for Sony's annual appearance at the Real Screen Conference located in Las Vegas targeting the Broadcasting sector.</div>
+          </div>
+
+          <div class="sample">
+            <h2>OuterspaceBook.net &ndash; Design &amp; Coding</h2>
+            <div class="clearer"></div>
+            <a href="https://www.outerspacebook.net" target="_blank"><img src="images/outerspacebook.png" alt="outerspacebook.net - an online guide to science fiction"></a><br>
+            <div class="flyout2" onClick="openWin('https://www.outerspacebook.net');"><span class="descriptions">VUE JS SPA (design and coding for this VUE JS website).</span></div>
+            <div class="clearer"></div>
+            <div class="mobile-text">VUE JS SPA (design and coding for this VUE JS website).</div>
+          </div>
+
+          <div class="spcr"></div>
+
+          <div class="sample">
+            <h2>Site Design/Development</h2>
+            <div class="clearer"></div>
+            <a href="https://www.aroundvernon.com" target="_blank"><img src="images/AroundVernon.png" width="280" height="233" alt="aroundvernon.com website"></a>
+            <div class="flyout2" onClick="openWin('https://www.aroundvernon.com');"><span class="descriptions">Around Vernon.com &mdash; Website design and development using HTML5, CSS3, SASS, Bootstrap, JQuery and PHP.</span></div>
+            <div class="clearer"></div>
+            <div class="mobile-text">Around Vernon.com &mdash; Website design and development using HTML5, CSS3, SASS, Bootstrap, JQuery and PHP.</div>
+          </div>
+
+          <div class="sample">
+            <h2>The Story behind the Around Vernon Website</h2>
+            <div class="clearer"></div>
+            <a href="https://www.gloriacurran.com/prototypes/around-vernon-styles.html" target="_blank"><img src="images/overview-around-vernon.png" width="280" height="233" alt="About the Creation of Around Vernon"></a><br>
+            <div class="flyout2" onClick="openWin('https://www.gloriacurran.com/prototypes/around-vernon-styles.html');"><span class="descriptions">Story behind the design/development of Around Vernon and its Stylesheet.</span></div>
+            <div class="clearer"></div>
+            <div class="mobile-text">Story behind the design/development of Around Vernon and its Stylesheet.</div>
+          </div>
+
+
+          <div class="sample">
+            <h2>Prototype - Around Vernon</h2>
+            <div class="clearer"></div>
+            <a href="https://www.gloriacurran.com/prototypes/proto-mobile-around-vernon.html" target="_blank"><img src="images/prototype-around-vernon.png" width="280" height="233" alt="Prototyping Around Vernon"></a><br>
+            <div class="flyout2" onClick="openWin('https://www.gloriacurran.com/prototypes/proto-mobile-around-vernon.html');"><span class="descriptions">High Fidelity Prototype Example for Around Vernon
+                using Adobe XD.</span></div>
+            <div class="clearer"></div>
+            <div class="mobile-text">High Fidelity Prototype Example for Around Vernon
+              using Adobe XD.</div>
+          </div>
+
+          <div class="spcr"></div>
+
+          <div class="sample">
+            <h2>Website Design/Coding - Jay Dee Trucking, Inc.</h2>
+            <div class="clearer"></div>
+            <a href="https://www.jaydeetrucking.com/jaydee/" target="_blank"><img src="images/jaydee.png" width="280" height="233" alt="Jay Dee Trucking"></a><br>
+            <div class="flyout2" onClick="openWin('https://www.jaydeetrucking.com/jaydee/');"><span class="descriptions">Jay Dee Trucking Site <br>(Using PHP, Bootstrap 4, Javascript, jQuery, CSS3).</span></div>
+            <div class="clearer"></div>
+            <div class="mobile-text">Jay Dee Trucking Site <br>(Using PHP, Bootstrap 4, Javascript, jQuery, CSS3).</div>
+          </div>
+
+
+          <div class="sample">
+            <h2>Prototype - Jay Dee Trucking, Inc.</h2>
+            <div class="clearer"></div>
+            <a href="https://gloriacurran.com/prototypes/proto-jaydee-trucking.html" target="_blank"><img src="images/prototyping-example-jaydee.png" width="280" height="233" alt="Prototype Jay Dee Trucking"></a><br>
+            <div class="flyout2" onClick="openWin('https://gloriacurran.com/prototypes/proto-jaydee-trucking.html');"><span class="descriptions">Prototype for Jay Dee Trucking using Adobe XD.<br></span></div>
+            <div class="clearer"></div>
+            <div class="mobile-text">Prototype for Jay Dee Trucking using Adobe XD.</div>
+          </div>
+
+          <div class="spcr"></div>
+
+          <div class="sample">
+            <h2>Sharp Solar Animation</h2>
+            <div class="clearer"></div>
+            <a href="https://www.gloriacurran.com/banner-sharp.html" target="_blank"><img src="images/storyboarding-sharp-solar.png" width="280" height="233" alt="Storyboarding"></a><br>
+            <div class="flyout2" onClick="openWin('https://www.gloriacurran.com/banner-sharp.html');"><span class="descriptions">Animate Banner with export to Canvas.</span></div>
+            <div class="clearer"></div>
+            <div class="mobile-text">Animate Banner with export to Canvas.</div>
+          </div>
+
+
+        </div><!--/flexbox-->
+
+      </div><!--/featured-->
+
+    </section><!--/content-->
+    <div class="clearer"></div>
+    <footer id="footer">
+      <div id="copyright">&copy; Copyright Gloria Curran <?php echo date("Y") ?></div>
+    </footer>
+    <div id="mobile">Would you like to view<br />the mobile web site?<br>
+      <input name="yes" value="Yes, Go to Mobile" type="button"> <br> <input name="no" value="No, Stay Here" type="button">
+    </div>
+  </div><!--/container-->
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      function is_touch_device() {
+        return 'ontouchstart' in window || navigator.maxTouchPoints;
+      };
+      if (is_touch_device()) {
+        $('html').removeClass('no-touch');
+        $('html').addClass('touch');
+      }
+
+      $('.sample').hover(
+        function() {
+          $(this).children('.flyout2').css("display", "block");
+        },
+        function() {
+          $(this).children('.flyout2').css("display", "none");
+        }
+      );
+
+    });
+
+    function openWin(thelocation) {
+      window.open(thelocation, '_blank');
+    }
+  </script>
+
+</body>
+
+</html>
